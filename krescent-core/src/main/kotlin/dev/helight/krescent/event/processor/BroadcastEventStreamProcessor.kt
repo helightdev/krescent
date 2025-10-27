e47dd336726d27448390da5827e53a840cb6560b
helight <contact@helight.dev>
2025-10-27 17:47:16 +0100

    Add support for processing SystemEvent with exception handling in BroadcastEventStreamProcessor

--- krescent-core/src/main/kotlin/dev/helight/krescent/event/processor/BroadcastEventStreamProcessor.kt
+++ krescent-core/src/main/kotlin/dev/helight/krescent/event/processor/BroadcastEventStreamProcessor.kt
@@ -3,14 +3,28 @@ package dev.helight.krescent.event.processor
 import dev.helight.krescent.HandlerChainParticipant
 import dev.helight.krescent.event.Event
 import dev.helight.krescent.event.EventStreamProcessor
+import dev.helight.krescent.event.SystemEvent
 import java.util.function.Consumer
 
 class BroadcastEventStreamProcessor(
     private val processors: List<EventStreamProcessor>,
 ) : EventStreamProcessor {
+
     override suspend fun process(event: Event) {
-        for (processor in processors) {
-            processor.process(event)
+        if (event is SystemEvent) {
+            val exceptions = mutableListOf<Exception>()
+            for (processor in processors) {
+                try {
+                    processor.process(event)
+                } catch (ex: Exception) {
+                    exceptions.add(ex)
+                }
+            }
+            if (exceptions.isNotEmpty()) throw SystemEventException(exceptions)
+        } else {
+            for (processor in processors) {
+                processor.process(event)
+            }
         }
     }
 
@@ -20,4 +34,8 @@ class BroadcastEventStreamProcessor(
             processor.accept(visitor)
         }
     }
+
+    class SystemEventException(
+        val exceptions: List<Exception>,
+    ) : Exception("Exceptions have occurred while processing a system event: ${exceptions.map { it.message }}")
 }
