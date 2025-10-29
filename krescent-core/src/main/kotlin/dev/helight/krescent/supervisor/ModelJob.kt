1709e9f6bf12da7779af4bb0b78f681564045fed
helight <helightdev@gmail.com>
2025-10-29 01:41:31 +0100

    Update `ModelJob.condition` to check `startupMutex` lock status in `ModelSupervisor`

--- krescent-core/src/main/kotlin/dev/helight/krescent/supervisor/ModelJob.kt
+++ krescent-core/src/main/kotlin/dev/helight/krescent/supervisor/ModelJob.kt
@@ -1,7 +1,7 @@
 package dev.helight.krescent.supervisor
 
 interface ModelJob {
-    suspend fun condition(supervisor: ModelSupervisor) = true
+    suspend fun condition(supervisor: ModelSupervisor) = !supervisor.startupMutex.isLocked
     suspend fun run(supervisor: ModelSupervisor)
     suspend fun onBefore(supervisor: ModelSupervisor) {}
     suspend fun onFailed(supervisor: ModelSupervisor, error: Throwable) {}
