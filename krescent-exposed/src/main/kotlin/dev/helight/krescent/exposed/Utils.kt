0b5588ac52893e41df9494c3037ea482c0bea25e
helight <helightdev@gmail.com>
2025-10-30 02:16:32 +0100

    Enhance `jdbcSuspendTransaction` to support transaction reuse for improved performance

--- krescent-exposed/src/main/kotlin/dev/helight/krescent/exposed/Utils.kt
+++ krescent-exposed/src/main/kotlin/dev/helight/krescent/exposed/Utils.kt
@@ -6,10 +6,19 @@ import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.runBlocking
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.Transaction
+import org.jetbrains.exposed.sql.transactions.TransactionManager
 import org.jetbrains.exposed.sql.transactions.transaction
 
-internal suspend fun <T> jdbcSuspendTransaction(database: Database, statement: suspend Transaction.() -> T): T =
+internal suspend fun <T> jdbcSuspendTransaction(
+    database: Database,
+    reuseTransaction: Boolean = false,
+    statement: suspend Transaction.() -> T,
+): T =
     coroutineScope {
+        if (reuseTransaction) TransactionManager.currentOrNull()
+            ?.takeIf { it.db == database }
+            ?.let { return@coroutineScope statement.invoke(it) }
+
         async(Dispatchers.IO) {
             transaction(database) {
                 runBlocking {
