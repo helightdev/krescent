21bf08ba09fd17b6a2aa62fb568cc85f9cd580a1
helight <helightdev@gmail.com>
2025-10-30 18:43:33 +0100

    Refactor jdbcSuspendTransaction to use newSuspendedTransaction for improved coroutine support

--- krescent-exposed/src/main/kotlin/dev/helight/krescent/exposed/Utils.kt
+++ krescent-exposed/src/main/kotlin/dev/helight/krescent/exposed/Utils.kt
@@ -1,13 +1,10 @@
 package dev.helight.krescent.exposed
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.runBlocking
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.Transaction
 import org.jetbrains.exposed.sql.transactions.TransactionManager
-import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 
 internal suspend fun <T> jdbcSuspendTransaction(
     database: Database,
@@ -19,11 +16,7 @@ internal suspend fun <T> jdbcSuspendTransaction(
             ?.takeIf { it.db == database }
             ?.let { return@coroutineScope statement.invoke(it) }
 
-        async(Dispatchers.IO) {
-            transaction(database) {
-                runBlocking {
-                    statement.invoke(this@transaction)
-                }
-            }
-        }.await()
+        return@coroutineScope newSuspendedTransaction(db = database) {
+            statement.invoke(this@newSuspendedTransaction)
+        }
     }
