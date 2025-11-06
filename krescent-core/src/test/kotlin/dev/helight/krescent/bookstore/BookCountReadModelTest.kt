b07b67c256d8525aec2a33dab333d6ea44972f71
helight <helightdev@gmail.com>
2025-11-06 04:24:13 +0100

    Add unit tests for `BooksAvailableReadModel` to validate `restoreOnly` and `catchup` behaviors

--- krescent-core/src/test/kotlin/dev/helight/krescent/bookstore/BookCountReadModelTest.kt
+++ krescent-core/src/test/kotlin/dev/helight/krescent/bookstore/BookCountReadModelTest.kt
@@ -1,22 +1,51 @@
 package dev.helight.krescent.bookstore
 
+import dev.helight.krescent.checkpoint.AlwaysCheckpointStrategy
+import dev.helight.krescent.checkpoint.impl.InMemoryCheckpointStorage
 import dev.helight.krescent.event.Event
 import dev.helight.krescent.model.EventModelBase.Extension.withConfiguration
 import dev.helight.krescent.model.ReadModelBase.Extension.catchup
+import dev.helight.krescent.model.ReadModelBase.Extension.restoreOnly
 import dev.helight.krescent.model.ReducingReadModel
 import dev.helight.krescent.source.impl.InMemoryEventStore
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.Serializable
 import java.util.concurrent.atomic.AtomicInteger
 import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
 
 class BookCountReadModelTest {
 
     @Test
-    fun `Test my other model shit`() = runBlocking {
+    fun `Test runs without exception`(): Unit = runBlocking {
         BooksAvailableReadModel().withConfiguration {
 
         }.catchup(InMemoryEventStore())
+    }
+
+    @Test
+    fun `Test restore only operation`(): Unit = runBlocking {
+        val source = InMemoryEventStore()
+        val checkpointStorage = InMemoryCheckpointStorage()
+        source.publishAll(bookstoreSimulatedEventStream)
+
+        val initial = BooksAvailableReadModel().withConfiguration {
+            useCheckpoints(checkpointStorage, AlwaysCheckpointStrategy)
+        }.restoreOnly()
+        assertNull(initial)
+
+        val caughtUp = BooksAvailableReadModel().withConfiguration {
+            useCheckpoints(checkpointStorage, AlwaysCheckpointStrategy)
+        }.catchup(source)
+        assertEquals(9, caughtUp.target["1"])
+
+        val late = BooksAvailableReadModel().withConfiguration {
+            useCheckpoints(checkpointStorage, AlwaysCheckpointStrategy)
+        }.restoreOnly()
+        assertNotNull(late)
+        assertEquals(9, late.target["1"])
     }
 }
 
